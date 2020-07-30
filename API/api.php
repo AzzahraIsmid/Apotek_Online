46ae58990a66f259c58bd413a6961ecca0230463
AzzahraIsmid <62925003+AzzahraIsmid@users.noreply.github.com>
2020-07-30 22:51:27 +0700

    update fungsi json

--- API/api.php
+++ API/api.php
@@ -1,9 +1,12 @@
 <?php
 
-	$servername = "localhost";
-	$database 	= "db_apotek";
-	$username 	= "root";
-	$password 	= "";
+$servername 	= "sql210.epizy.com";
+
+$database 	= "epiz_26390060_db_apotek";
+
+$username 	= "epiz_26390060";
+
+$password 	= "Rv7SQm9m3H";
 
 	$connect 	= new mysqli($servername, $database, $username, $password);
 
