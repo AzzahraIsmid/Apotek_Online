9485df9e0586681321aeacbab621b6e003fccec7
Arisdadwiputri <63054213+Arisdadwiputri@users.noreply.github.com>
2020-08-13 21:22:33 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+$servername = "sql210.epizy.com";
+
+$database 	= "epiz_26390060_db_apotek";
+
+$username 	= "epiz_26390060";
+
+$password 	= "Rv7SQm9m3H";
+
+
+
+$connect 	= new mysqli($servername,$username,$password,$database);
+
+
+
+if (!$connect) {
+
+    die("Connection failed: " . mysqli_connect_error());
+
+}
+
+
+
+?>
